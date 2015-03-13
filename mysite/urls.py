c2197b773a6ed7d30ba98975472c5205ac26ef12
scbhooshan <scbhooshan@gmail.com>
2015-03-12 23:45:49 -0700

    static folder with login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,6 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     url(r'^rallybench/', include('rallybench.urls')),
-    url(r'^poll/', include('poll.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
 
