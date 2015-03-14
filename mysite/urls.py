b9e33e0fe70f4bf9eeb7a8414674b3f036c45276
scbhooshan <scbhooshan@gmail.com>
2015-03-13 21:36:28 -0700

    url change css html admin

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'rallybench.views.login'), 
     url(r'^rallybench/', include('rallybench.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- rallybench/admin.py
+++ rallybench/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from rallybench.models import RallyTask, RallyUser, RallyUserSession, Deployment
 
 # Register your models here.
+admin.site.register(RallyTask)
+admin.site.register(RallyUser)
+admin.site.register(RallyUserSession)
+admin.site.register(Deployment)
